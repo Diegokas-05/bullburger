3141b2acff2c6225aa7586b6726f75f6e0aadbae
JONATHAN5594 <jonathan.fuentes@catolica.edu.sv>
2025-10-22 10:47:00 -0600

    mis cambios base y lista clientes

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path('administrador/dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('empleado/dashboard/', views.empleado_dashboard, name='empleado_dashboard'),
     path('cliente/dashboard/', views.cliente_dashboard, name='cliente_dashboard'),
+
+    path('administrador/clientes/', views.lista_clientes, name='lista_clientes'),
+    path('clientes/editar/<int:id>/', views.editar_cliente, name='editar_cliente'),
+    path('clientes/eliminar/<int:id>/', views.eliminar_cliente, name='eliminar_cliente'),
+
 ]
