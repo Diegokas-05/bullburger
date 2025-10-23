d6c53b11dc7f45f7c6ab202a3057e6dd7f7a9bd8
Ana <ana.villeda@catolica.edu.sv>
2025-10-22 23:11:56 -0600

    Agregando de vista y estilos de listas empleados junto a ajustes de css

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -11,4 +11,12 @@ urlpatterns = [
     path('administrador/dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('empleado/dashboard/', views.empleado_dashboard, name='empleado_dashboard'),
     path('cliente/dashboard/', views.cliente_dashboard, name='cliente_dashboard'),
+    path('administrador/clientes/', views.lista_clientes, name='lista_clientes'),
+    path('clientes/editar/<int:id>/', views.editar_cliente, name='editar_cliente'),
+    path('clientes/eliminar/<int:id>/', views.eliminar_cliente, name='eliminar_cliente'),
+    path('administrador/empleados/', views.lista_empleados, name='lista_empleados'),
+    path('empleados/editar/<int:id>/', views.editar_empleado, name='editar_empleado'),
+    path('empleados/eliminar/<int:id>/', views.eliminar_empleado, name='eliminar_empleado'),
+
+
 ]
